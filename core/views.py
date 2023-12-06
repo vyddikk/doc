dcf9f5b6867aea78afbb23fed0e9a302ed43a65b
vyddikk <vyddikkk@yandex.ru>
2023-12-06 21:00:49 +0300

    start app

--- core/views.py
+++ core/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
+from blog.models import Post
+
 # Create your views here.
+
+
+def frontpage(request):
+    posts = Post.objects.all()
+
+    return render(request, 'core/frontpage.html', {'posts': posts})
+
+
+def about(request):
+    return render(request, 'core/about.html')
